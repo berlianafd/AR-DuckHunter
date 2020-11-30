ba7bc2374ab0b172a6023a7f8e9ce8568aa565b8
Berliana Farah <42767850+berlianafd@users.noreply.github.com>
2020-11-30 22:58:40 +0700

    Pertemuan14

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Vuforia;
 
 public class GameController : MonoBehaviour
 {
  public static GameController instance;
 
- 
+ //aturan ronde
+ private int roundTargetScore = 3;
+ public int roundScore = 0;
+ private int scoreIncrement = 2;
+ public bool playerStarted = false;
+
  public GameObject startPanel;
  public int playerScore = 0;
  public Text hitungTeks;
@@ -30,6 +36,7 @@ public class GameController : MonoBehaviour
  public GameObject GUIGameOverPanel;
  public GameObject GUIStartPanel;
  public GameObject Terrain;
+ public GameObject GUIGun;
  
 
  AudioSource audio;
@@ -41,5 +48,67 @@ public class GameController : MonoBehaviour
      {
          instance = this;
      }
+ }
+
+ void Start(){
+ 	playerScore = int.Parse(hitungTeks.text);
+ 	showStartPanel();
+ 	audio = GetComponent<AudioSource>();
+ 	hitungNyawa.text = nyawa.ToString();
+ }
+
+ void Update(){
+ 	if(DefaultTrackableEventHandler.trueFalse == true){
+ 		hideStartPanel();
+ 		showItems();
+
+ 		if(playerStarted == false){
+ 			StartCoroutine(playRound());
+ 		}
+
+ 		playerStarted = true;
+ 	} else {
+ 		showStartPanel();
+ 		hideItems();
+ 	}
+ }
+
+ public IEnumerator playRound(){
+ 	yield return new WaitForSeconds(2f);
+ 	targetTeks.text = "Tembak "+tembakPerRonde+" burung";
+ }
+
+ private void showStartPanel(){
+ 	startPanel.SetActive(true);
+ }
+
+ private void hideStartPanel(){
+ 	startPanel.SetActive(false);
+ }
+
+ private void showItems(){
+ 	GUITeksSkor.SetActive(true);
+ 	GUITeksNyawa.SetActive(true);
+ 	GUITargetBidikan.SetActive(true);
+ 	GUITembak.SetActive(true);
+ 	GUIAnjing.SetActive(true);
+ 	GUITeksRonde.SetActive(true);
+ 	GUIGameOverPanel.SetActive(true);
+ 	GUIGun.SetActive(true);
+ 	//hideStartPanel();
+ 	Terrain.SetActive(true);
+ }
+
+ private void hideItems(){
+ 	GUITeksSkor.SetActive(false);
+ 	GUITeksNyawa.SetActive(false);
+ 	GUITargetBidikan.SetActive(false);
+ 	GUITembak.SetActive(false);
+ 	GUIAnjing.SetActive(false);
+ 	GUITeksRonde.SetActive(false);
+ 	GUIGameOverPanel.SetActive(false);
+ 	GUIGun.SetActive(false);
+ 	//hideStartPanel();
+ 	Terrain.SetActive(false);
  }
 }
